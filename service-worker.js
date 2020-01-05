f0ac4c5a80fbb2788ba50918a689b5c6bf4f847a
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-05 16:28:29 +0100

    activate service worker

--- service-worker.js
+++ service-worker.js
@@ -25,3 +25,10 @@ self.addEventListener('install', event => {
     );
     console.log('Service  Worker installing...');
 });
+
+
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activating.');
+});
+
